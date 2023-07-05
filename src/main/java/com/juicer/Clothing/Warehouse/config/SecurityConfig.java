b5c6372929d7da9d89bbb14d66a7086a039bba8a
Sebastian Paul Muzalewski <113144733+SebastianMuzalewski@users.noreply.github.com>
2023-07-05 00:22:44 -0400

    Fixed first time login bug

--- src/main/java/com/juicer/Clothing/Warehouse/config/SecurityConfig.java
+++ src/main/java/com/juicer/Clothing/Warehouse/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.juicer.Clothing.Warehouse.config;
 
-import com.juicer.Clothing.Warehouse.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +10,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.juicer.Clothing.Warehouse.model.User;
+import com.juicer.Clothing.Warehouse.repository.UserRepository;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 public class SecurityConfig {
@@ -20,39 +22,41 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
+    public UserDetailsService userDetailsService(UserRepository userRepository){
         return username -> {
             User user = userRepository.findByUsername(username);
-            if (user != null){
+            if(user !=null){
                 return user;
             }
             throw new UsernameNotFoundException(username);
         };
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests()
-                .requestMatchers("/home","/design","itemList","/about")
+                .requestMatchers(toH2Console()).permitAll()
+                .requestMatchers("/design", "/itemList", "/about")
                 .hasRole("USER")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
-
                 .and()
                 .csrf()
-                .ignoringRequestMatchers("/h2-console/***")
-
+                .ignoringRequestMatchers(toH2Console())
+                .ignoringRequestMatchers("/design")
                 .and()
                 .headers()
                 .frameOptions()
                 .sameOrigin()
-
                 .and()
                 .build();
     }
 }
+
